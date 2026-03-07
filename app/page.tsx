6e3078e5b979e88d50aaf2c8a033874d2cb37036
Doniyor <doniyoryusupob246@gmail.com>
2026-03-07 21:51:37 +0300

    deo

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,90 @@
-import Image from "next/image";
+'use client';
+import { RangeSlider } from '@/components/shared/range-slider';
+import { Button } from '@/components/ui/button';
+import { Label } from '@/components/ui/label';
+import { RadioGroup, RadioGroupItem } from '@/components/ui/radio-group';
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select';
+import React from 'react';
+
+const rooms = ['1', '2', '3', '4', '5+'];
 
 export default function Home() {
+  const [isActiveBtn, setIsActiveBtn] = React.useState<string[]>([]);
+  const toggleRoom = (room: string) => {
+    setIsActiveBtn((prev) =>
+      prev.includes(room) ? prev.filter((r) => r !== room) : [...prev, room],
+    );
+  };
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <div className="max-w-107.5 w-full mx-auto px-4 pt-5">
+      <div className="w-50 mx-auto text-center">
+        <h3 className="text-[14px] font-bold leading-3 mb-2">MyPropertyBot</h3>
+        <p className="font-light leading-2 text-[10px]">мини-приложение</p>
+      </div>
+      <div className="flex flex-col ">
+        <div className="border rounded-full border-black py-1 px-3 w-[200px] text-center mt-10 mx-auto">
+          <p className="text-[16px]">Создать фильтр</p>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+
+        <RadioGroup className="flex gap-5 justify-center mt-8" defaultValue="Аренда квартир">
+          <div className="flex items-center gap-3">
+            <RadioGroupItem value="Аренда квартир" id="Аренда квартир" />
+            <Label htmlFor="Аренда квартир">Аренда квартир</Label>
+          </div>
+          <div className="flex items-center gap-3">
+            <RadioGroupItem value="Аренда домов" id="Аренда домов" />
+            <Label htmlFor="Аренда домов">Аренда домов</Label>
+          </div>
+        </RadioGroup>
+
+        <div className="flex justify-between items-center max-w-[430px] w-full mt-10">
+          <h3 className="font-medium">Район</h3>
+          <Select>
+            <SelectTrigger className="w-[280px] rounded-full border-2">
+              <SelectValue placeholder="Выберите район" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectGroup>
+                <SelectItem value="light">Light</SelectItem>
+                <SelectItem value="dark">Dark</SelectItem>
+                <SelectItem value="system">System</SelectItem>
+              </SelectGroup>
+            </SelectContent>
+          </Select>
         </div>
-      </main>
+
+        <div className="mt-10 w-full pb-7">
+          <div className="mb-4">
+            <h3 className="font-medium">Цена ($)</h3>
+          </div>
+
+          <div className="w-[80%] mx-auto">
+            <RangeSlider min={0} max={1000} step={10} value={[0, 1000]} />
+          </div>
+        </div>
+
+        <div>
+          <h3 className="font-medium">Комнаты</h3>
+          <div className="flex justify-between mt-2">
+            {rooms.map((room, i) => (
+              <Button
+                onClick={() => toggleRoom(room)}
+                key={room}
+                variant={isActiveBtn.includes(room) ? 'default' : 'outline'}
+                className="w-[50px] h-[50px]">
+                {room}
+              </Button>
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
